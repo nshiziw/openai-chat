b6174e20eea4f4542c02867a9823252c3038f5f8
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-10 12:31:14 +0200

    Installing Shadcn UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import ChatUI from "./components/ChatUI.jsx"
+const App = () => {
+  return (
+    <>
+      <ChatUI />
+    </>
+  )
+}
+
+export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@/components": path.resolve(__dirname, "src/components"),
+    },
   },
-  plugins: [],
-}
+})
